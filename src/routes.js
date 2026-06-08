50cff935a4b4e30854f24055940049509bca9704
Caio Superheroi <parolincaio@gmail.com>
2026-06-08 17:50:20 -0300

    bycrypt

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
 
 const routes = express.Router();
 
